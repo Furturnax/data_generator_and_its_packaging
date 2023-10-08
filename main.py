34a4b2a61558ea08885056ea2241c3e04ebe4aee
Andrew Fedorchenko <furturnax@gmail.com>
2023-10-08 13:45:16 +0300

    Add zip function and now we can create files and after archive

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
 import pandas as pd
 from mimesis import Generic
 from mimesis.locales import Locale
-
-
-# class ImportData:
-#     """
-#     Печатает диалоговое онко и предлагает
-#     импортировать или создать файл с данными.
-#     """
-
-#     def chois():
-#         print('Привет. Я программа способная создать архив с '
-#               'твоими данными :3\n'
-#               'Давай выберем, что необходимо заархивировать\n'
-#               '1. Загрузить файл с компьютера.\n'
-#               '2. Записать данные самостоятельно.\n'
-#               '3. Сгенерировать данные.')
-
-#         while True:
-#             try:
-#                 user_choise = int(input('Напиши цифру нужного действия: '))
-#             except ValueError:
-#                 print('Нужна цифра.')
-
-#             if user_choise == 1:
-#                 print('Пока в разработке')
-#                 break
-#             elif user_choise == 2:
-#                 print('Пока в разработке')
-#                 break
-#             elif user_choise == 3:
-#                 print('Пока в разработке')
-#                 break
-#             else:
-#                 print('Извини, но действие не выполнено, давай ещё раз.')
-
-
-
-NAME_FILE: str = 'DataFrame'
+import zipfile
+from py7zr import SevenZipFile
 
 
 class GeneratorData:
@@ -90,8 +55,9 @@ class GeneratorData:
 class ExportData(GeneratorData):
     """Экспорт данных в файл."""
 
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, user_input):
         self.dataframe = dataframe
+        self.user_input: int = user_input
 
     def export_to_excel(self, filename=None):
         """Экспорт сгенерированной таблицы данных в файл Excel."""
@@ -122,41 +88,165 @@ class ExportData(GeneratorData):
 
 
 class ZipData:
-    pass
+    """Архивация данных в файл."""
+
+    def create_zip(output_filename, *files_to_archive):
+        with zipfile.ZipFile(output_filename, 'w',
+                             zipfile.ZIP_DEFLATED) as zip:
+            for file_to_archive in files_to_archive:
+                zip.write(file_to_archive)
+
+    def create_7z(output_filename, *files_to_archive):
+        with SevenZipFile(output_filename, 'w') as sz:
+            for file_to_archive in files_to_archive:
+                sz.write(file_to_archive)
 
 
-user_input: int = int(input('Введите желаемое количество строк: '))
-GeneratorData().input_number = GeneratorData().write_num_rows(user_input)
-
-data_exporter = ExportData(GeneratorData().generate_dataframe())
-data_exporter.export_to_csv(f'{NAME_FILE}.csv')
-data_exporter.export_to_txt(f'{NAME_FILE}.txt')
-data_exporter.export_to_excel(f'{NAME_FILE}.xlsx')
-
-
+def menu():
+    print('\nПривет. Я программа способная создать архив с '
+          'твоими данными :3\n'
+          'Давай выберем, нужное действие:\n'
+          '\n1. Загрузить файл с компьютера.\n'
+          '2. Записать данные самостоятельно.\n'
+          '3. Сгенерировать данные.\n')
 
 
-# if __name__ == "__main__":
-    # ImportData.chois()
-    # data_exporter = ExportData(GeneratorData.generate_dataframe(50))
-    # print(GeneratorData.generate_dataframe(50))
-    # data_exporter.export_to_csv(f'{}.csv')
-    # data_exporter.export_to_txt(f'{}.txt')
-    # data_exporter.export_to_excel(f'{}.xlsx')
+def file_format_choise():
+    print('\nВ каком кормате хотите сохранить данные?\n'
+          '1. В формате Excel.\n'
+          '2. В формате CSV\n'
+          '3. В формате TXT\n'
+          '4. Все форматы сразу\n')
+
+    while True:
+        try:
+            file_format: int = int(input('Выберети номер действия: '))
+        except ValueError:
+            print('Нужна цифра.')
+
+        if file_format == 1:
+            data_exporter.export_to_excel(f'{NAME_FILE}.xlsx')
+            zipping('xlsx')
+            break
+        elif file_format == 2:
+            data_exporter.export_to_csv(f'{NAME_FILE}.csv')
+            zipping('csv')
+            break
+        elif file_format == 3:
+            data_exporter.export_to_txt(f'{NAME_FILE}.txt')
+            zipping('txt')
+            break
+        elif file_format == 4:
+            data_exporter.export_to_excel(f'{NAME_FILE}.xlsx')
+            data_exporter.export_to_csv(f'{NAME_FILE}.csv')
+            data_exporter.export_to_txt(f'{NAME_FILE}.txt')
+            zipping('all')
+            break
+        else:
+            print('Извини, но действие не выполнено, давай ещё раз.')
 
 
+def zipping(*formats):
+    print('\nАрхивируем данные?\n'
+          '1. Да.\n'
+          '2. Нет.\n')
+    while True:
+        try:
+            choise_archive: int = int(input('Выберети номер действия: '))
+        except ValueError:
+            print('Нужна цифра.')
 
-    # def write_num_rows() -> int:
-    #     """Проверка введенного значения строк на корректность."""
-    #     while True:
-    #         try:
-    #             value_num_rows: int = int(input('Введите желаемое'
-    #                                             'количество строк: '))
-    #         except ValueError:
-    #             print('Нужно значение в виде цифры.')
+        if choise_archive == 1:
+            NAME_ZIP: str = str(input('Введите имя архива: '))
+            print('\nВыберите формат архивации:\n'
+                  '1. Zip.\n'
+                  '2. 7z.\n')
+            while True:
+                try:
+                    type_zip: int = int(input('Выберети номер действия: '))
+                except ValueError:
+                    print('Нужна цифра.')
 
-    #         if value_num_rows <= 2_000_000:
-    #             return value_num_rows
-    #         raise ValueError(
-    #             'Количество строк больше 2 000 000. Экспорт не выполнится.'
-    #             )
+                while True:
+                    if type_zip == 1:
+                        for format in formats:
+                            if format == 'xlsx':
+                                ZipData.create_zip(f'{NAME_ZIP}.zip',
+                                                   f'{NAME_FILE}.xlsx')
+                            elif format == 'csv':
+                                ZipData.create_zip(f'{NAME_ZIP}.zip',
+                                                   f'{NAME_FILE}.csv')
+                            elif format == 'txt':
+                                ZipData.create_zip(f'{NAME_ZIP}.zip',
+                                                   f'{NAME_FILE}.txt')
+                            elif format == 'all':
+                                ZipData.create_zip(f'{NAME_ZIP}.zip',
+                                                   f'{NAME_FILE}.xlsx',
+                                                   f'{NAME_FILE}.csv',
+                                                   f'{NAME_FILE}.txt')
+                            else:
+                                print('Ошибка, таких форматов нет.')
+                        print('Архив создан.')
+                        print('Спасибо, что воспользовались моей программой.')
+                        break
+                    elif type_zip == 2:
+                        for format in formats:
+                            if format == 'xlsx':
+                                ZipData.create_7z(f'{NAME_ZIP}.7z',
+                                                  f'{NAME_FILE}.xlsx')
+                            elif format == 'csv':
+                                data_exporter.export_to_csv(f'{NAME_FILE}.csv')
+                                ZipData.create_7z(f'{NAME_ZIP}.7z',
+                                                  f'{NAME_FILE}.csv')
+                            elif format == 'txt':
+                                ZipData.create_7z(f'{NAME_ZIP}.7z',
+                                                  f'{NAME_FILE}.txt')
+                            elif format == 'all':
+                                ZipData.create_7z(f'{NAME_ZIP}.7z',
+                                                  f'{NAME_FILE}.xlsx',
+                                                  f'{NAME_FILE}.csv',
+                                                  f'{NAME_FILE}.txt')
+                            else:
+                                print('Ошибка, таких форматов нет')
+                        print('Архив создан.')
+                        print('Спасибо, что воспользовались моей программой.')
+                        break
+                    else:
+                        print('Такого действия нет.')
+
+        elif choise_archive == 2:
+            data_exporter.export_to_excel(f'{NAME_FILE}.xlsx')
+            data_exporter.export_to_csv(f'{NAME_FILE}.csv')
+            data_exporter.export_to_txt(f'{NAME_FILE}.txt')
+            print('Спасибо, что воспользовались моей программой.')
+            break
+        else:
+            print('Такого действия нет.')
+
+
+if __name__ == "__main__":
+    menu()
+    while True:
+        try:
+            user_choise: int = int(input('Напиши цифру нужного действия: '))
+        except ValueError:
+            print('Нужна цифра.')
+
+        if user_choise == 1:
+            print('Пока в разработке.')
+            break
+        elif user_choise == 2:
+            print('Пока в разработке.')
+            break
+        elif user_choise == 3:
+            user_input: int = int(input('Введите желаемое количество строк: '))
+            GeneratorData().input_number = GeneratorData().write_num_rows(
+                user_input
+                )
+            data_exporter = ExportData(
+                GeneratorData().generate_dataframe(), user_input
+                )
+            NAME_FILE = str(input('Как назовём файл? '))
+            file_format_choise()
+        else:
+            print('Извини, но действие не выполнено, давай ещё раз.')
